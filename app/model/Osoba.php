1d6022484cea09b5fd33691f3dca2ab53b47cd8a
Marko Pavlović <markopavlovic316@gmail.com>
2023-02-21 15:35:25 +0100

    Autoload function that is pulling files directly from defined sources (core,controller,model)

---
+++ app/model/Osoba.php
@@ -0,0 +1,8 @@
+<?php
+
+class Osoba{
+    public function __construct()
+    {
+        echo 'Hello from Osoba constructor'.'<br>';
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo 'Hello';
+define('BP', __DIR__ . DIRECTORY_SEPARATOR);
+define('BP_APP', BP. 'app' . DIRECTORY_SEPARATOR);
+
+$zaAutoLoad=[
+  BP_APP . 'model',
+  BP_APP . 'controller',
+  BP_APP . 'core'
+];
+
+$putanje = implode(PATH_SEPARATOR,$zaAutoLoad);
+
+set_include_path($putanje);
+
+spl_autoload_register(function($klasa){
+  $putanje = explode(PATH_SEPARATOR, get_include_path());
+  foreach($putanje as $putanja){
+    echo $putanja . '<br>';
+    $datoteka = $putanja . DIRECTORY_SEPARATOR . $klasa . '.php';
+    echo $datoteka, '<br>';
+    if(file_exists($datoteka)){
+      require_once $datoteka;
+      break;
+    }
+  }
+});
+
+new Osoba();
